55a2ec93f0583221b79d7c502506c49aa7b2c32e
Allan Fernandes <allan.jesus@e-unicred.com.br>
2025-03-21 16:26:58 -0300

    feat: Swagger

--- src/main/java/br/com/foursales/auth_service/web/controller/swagger/AuthControllerDoc.java
+++ src/main/java/br/com/foursales/auth_service/web/controller/swagger/AuthControllerDoc.java
@@ -1,2 +1,19 @@
-package br.com.foursales.auth_service.web.controller.swagger;public interface AuthControllerDoc {
+package br.com.foursales.auth_service.web.controller.swagger;
+
+import br.com.foursales.auth_service.application.dto.auth.AuthRequestDTO;
+import br.com.foursales.auth_service.application.dto.auth.AuthResponseDTO;
+import br.com.foursales.auth_service.web.annotations.DefaultSwaggerMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "Authenticate", description = "Endpoints para autenticação e autorização")
+public interface AuthControllerDoc {
+
+    @PostMapping("/login")
+    @DefaultSwaggerMessage
+    @Operation(summary = "Autentica um usuário e retorna um token JWT")
+    ResponseEntity<AuthResponseDTO> login(@RequestBody AuthRequestDTO request);
 }
--- src/main/java/br/com/foursales/auth_service/web/controller/swagger/UserControllerDoc.java
+++ src/main/java/br/com/foursales/auth_service/web/controller/swagger/UserControllerDoc.java
@@ -1,2 +1,19 @@
-package br.com.foursales.auth_service.web.controller.swagger;public interface UserControllerDoc {
+package br.com.foursales.auth_service.web.controller.swagger;
+
+import br.com.foursales.auth_service.application.dto.user.UserRequestDTO;
+import br.com.foursales.auth_service.application.dto.user.UserResponseDTO;
+import br.com.foursales.auth_service.web.annotations.DefaultSwaggerMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "Register", description = "Endpoints criar usuario.")
+public interface UserControllerDoc {
+
+    @PostMapping("/register")
+    @DefaultSwaggerMessage
+    @Operation(summary = "Cria um usuário")
+    ResponseEntity<UserResponseDTO> register(@RequestBody UserRequestDTO request);
 }
